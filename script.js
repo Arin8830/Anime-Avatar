4cdc4fc21242b8d0c629f75d3570dc4e88fa039a
Arin8830 <149166233+Arin8830@users.noreply.github.com>
2023-11-15 23:14:42 +0530

    index.html

---
+++ script.js
@@ -0,0 +1,11 @@
+function imgslider(anything){
+  document.querySelector('.arm').src = anything;
+}
+
+// hamberger menu
+
+function onClickMenu(){
+  document.getElementById("Menu").classList.toggle("icon");
+  document.getElementById("navme").classList.toggle("change");
+}
+
